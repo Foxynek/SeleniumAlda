d7da2d9f7b5e8f2ae4eea6e2549cdd31b475bc83
Foxynek <hack7@seznam.cz>
2018-03-10 15:55:14 +0100

    Pom structure set

--- src/test/java/Tests/OpenURLAndLogin.java
+++ src/test/java/Tests/OpenURLAndLogin.java
@@ -3,10 +3,8 @@ package Tests;
 
 import Pages.AfterLoginPage;
 import Pages.BasePage;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import java.util.concurrent.TimeUnit;
